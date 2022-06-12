d91feb3629e0f85b183290f1f52973d70ee63d59
Simeon Simeonov <simeonsimeonov006@gmail.com>
2022-06-12 12:06:23 +0300

    Added about page.

--- src/main/java/com/example/pathfinder/web/controller/CommonController.java
+++ src/main/java/com/example/pathfinder/web/controller/CommonController.java
@@ -4,10 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class CommonController {
 
     @GetMapping("/")
     public String homeView() {
         return "index";
+    }
+
+    @GetMapping("/about")
+    public String aboutView() {
+        return "about";
     }
 }
